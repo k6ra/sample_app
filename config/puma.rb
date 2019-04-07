229cf0dce90e9a1f8e18a7cf77baa1cdd8a728b9
k6ra <m98213@gmail.com>
2019-04-07 15:09:05 +0900

    fix puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -60,7 +60,7 @@ threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
 preload_app!
-rackup      DefaultRackup
+#rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
